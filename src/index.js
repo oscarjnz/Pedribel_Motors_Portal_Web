41c6a35d4199b4830589f43d79a543d6fb7037fb
Oscar Jiménez <ojimenez6@est.unibe.edu.do>
2025-08-13 16:10:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === "/motos") {
-      const motos = [
-        { id: 1, marca: "Honda", modelo: "CBR 500", precio: 8000, descripcion: "Moto deportiva de media cilindrada." },
-        { id: 2, marca: "Yamaha", modelo: "MT-07", precio: 7500, descripcion: "Estilo naked con gran versatilidad." }
-      ];
+      // Consultar todas las motos
+      const { results } = await env.DB.prepare("SELECT * FROM motos").all();
 
-      return new Response(JSON.stringify(motos), {
+      return new Response(JSON.stringify(results), {
         headers: { "content-type": "application/json; charset=utf-8" }
       });
     }
