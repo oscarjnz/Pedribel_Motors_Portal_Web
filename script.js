b4edfa955d88a804cd3d8900766e68b7a5677ba4
Oscar Jiménez <ojimenez6@est.unibe.edu.do>
2025-08-18 10:02:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -190,3 +190,24 @@ function setupIntersectionObserver() {
         observer.observe(el);
     });
 }
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_kgh1w9x';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
