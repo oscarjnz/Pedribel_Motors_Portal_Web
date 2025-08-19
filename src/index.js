6171c51c6ee151a13e1919337611d098a0ebe77d
Oscar Jiménez <ojimenez6@est.unibe.edu.do>
2025-08-19 11:29:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 export default {
   async fetch(request, env) {
-    const { pathname } = new URL(request.url);
-
-    if (pathname === "/api/x1000") {
-      // If you did not use `DB` as your binding name, change it here
-      const { results } = await env.catalogo_db.prepare(
-        "SELECT * FROM Productos WHERE Marca = ?"
-      )
-        .bind("X1000")
-        .run();
-      return new Response(JSON.stringify(results), {
-        headers: { 'Content-Type': 'application/json' }
-      });
+    const url = new URL(request.url);
+    const key = url.pathname.slice(1);
+    switch (request.method) {
+      case "PUT":
+        await env.pedribel-motors.put(key, request.body);
+        return new Response(`Put ${key} successfully!`);
+      default:
+        return new Response(`${request.method} is not allowed.`, {
+          status: 405,
+          headers: {
+            Allow: "PUT",
+          },
+        });
     }
-
-    return new Response(
-      "Call /api/beverages to see everyone who works at X1000"
-    );
   },
 };
+
 
